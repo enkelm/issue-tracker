70b0417fa9b1831f2a34420d88bf63c035ab2185
enkelm <enkel.murati33@gmail.com>
2023-11-25 22:58:17 +0100

    feat(issues): add loading pages and finish issue detail page design

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,23 +1,30 @@
+import { IssueStatusBadge } from "@/app/components"
 import prisma from "@/prisma/client"
+import { Card, Flex, Heading, Text } from "@radix-ui/themes"
 import { notFound } from "next/navigation"
+import Markdown from "react-markdown"
 
 type Props = {
   params: { id: string }
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
-  if (typeof params.id !== "number") notFound()
-
   const issue = await prisma.issue.findUnique({ where: { id: parseInt(params.id) } })
 
   if (!issue) notFound()
 
+  setTimeout(() => {}, 2000)
+
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <Heading>{issue.title}</Heading>
+      <Flex gap="3" my="2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose mt-4">
+        <Markdown>{issue.description}</Markdown>
+      </Card>
     </div>
   )
 }
